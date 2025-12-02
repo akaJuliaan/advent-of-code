763ebbdda10240e55afaa97cedd1619dab54348e
akaJuliaan <hi@julian.at>
2025-12-02 08:34:36 +0000

    day 2 part 1

---
+++ 2025/02/1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+invalidIDs = 0
+
+with open('input.txt', 'r') as f:
+    line = f.readline()
+    for idRange in line.split(','):
+        splitID = idRange.split('-')
+        firstID = int(splitID[0])
+        lastID = int(splitID[1])
+
+        for ID in range(firstID, lastID+1):
+            number = ''
+            for i, digit in enumerate(str(ID)):
+                number += digit
+                if number == str(ID)[i+1:]:
+                    print(f"Found invalid ID: {ID}")
+                    invalidIDs += ID
+                    continue
+
+print(f'Sum of all invalid IDs: {invalidIDs}')
