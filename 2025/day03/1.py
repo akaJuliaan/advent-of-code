f30e1d177f1a5842affa404bf61d67e9f6c9822a
akaJuliaan <hi@julian.at>
2025-12-03 12:22:23 +0000

    cleanup

--- 2025/day03/1.py
+++ 2025/day03/1.py
@@ -3,15 +3,16 @@
 joltage = 0
 
 with open('input.txt', 'r') as f:
-    for line in f.readlines():
+    for ln in f.readlines():
+        line = ln.strip()
         first = max(line)
         index = line.find(first)
-        if (index + 2) == len(line):
+        if (index + 1) == len(line):
             second = first
             first = max(line[:index])
         else:
             second = max(line[index+1:])
 
-        joltage += int(first)*10 + int(second)
+        joltage += int(first) * 10 + int(second)
         
 print(f"Sum of max joltage {joltage}")
